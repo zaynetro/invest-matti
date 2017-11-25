bf8806b782744fe8dca00557e1b75dac02b7a98e
Farbod Faghihi <faghihifa@gmail.com>
2017-11-25 16:51:13 +0200

    first plot type done

--- app.R
+++ app.R
@@ -50,29 +50,40 @@ ui <- dashboardPage(
   body
 )
 
+get_investment <- function(principle, years, vector_length= 365, interest_rate = 0.04, method='test') {
+  return(seq(principle, (principle*(1+(interest_rate/vector_length))^(years*vector_length)), length.out = years*vector_length))
+}
+
 server <- function(input, output) {
-  set.seed(122)
-  actual <- rnorm(100) * 50 + 5000
-  investment <- rnorm(100) * 50 + 5000 + cumsum(rnorm(100) * 50 + 10)
-  timestamps <- seq( as.Date("2017-07-01"), by=1, len=100)
-  finances <- data.table(date = rep(x = timestamps, times=2), 
+  set.seed(6)
+  vector_length <- 365
+  years <- 20
+  actual <- c(6000)
+  for(i in 2:vector_length) {
+    actual <- c(actual, (actual[length(actual)] +
+                           sample(c(0, rnorm(1, 0, 1)[1] * 50),
+                                  1,
+                                  runif(1,0.7,0.9)))
+                )
+  }
+  actual <- c(actual, rep(actual[vector_length], times=(years*vector_length)))
+  investment <- get_investment(principle = actual[vector_length],
+                               years = years,
+                               vector_length = vector_length)
+  print(length(investment))
+  timestamps <- seq( as.Date("2016-07-01"), by=1, len=((years+1)*vector_length))
+  finances <- data.table(date = rep(x = c(timestamps, timestamps[(vector_length+1):length(timestamps)])), 
                          balance=c(actual, investment),
-                         type=c(rep("Current", times=100), rep("Investment", times=100)))
-  
+                         type=c(rep("Current", times=((years+1)*vector_length)), 
+                                rep("Investment", times=(years*vector_length))))
+  print(finances)
   output$balance_line_graph <- renderPlot({
     print(
       ggplot(data = finances, aes(x=date, y=balance, color=type, linetype=type)) + 
         geom_line() +
-        geom_smooth(method = "lm", formula = y ~ exp(x)) +
-        # scale_y_continuous(limits=c(0, 75000)) +
-        # scale_color_manual(name="", values=c(prediction="blue", actual="red"), labels=c("Investment Plan", "Current")) +
-        # scale_linetype_manual(name="", values=c(prediction=3, actual=1), labels=c("Investment Plan", "Current")) +
-        # theme_minimal() +
+        geom_smooth(method = "glm") +
         labs(x="Date", y="Total")
     )
-    # data <- histdata[seq_len(input$slider)]
-    # plot(x = timestamps, y=balance, col='red') +
-    # lines(x = timestamps, y=balance, col = 'steelblue')
   })
 }
 
